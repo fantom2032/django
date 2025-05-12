e6070198aa8ec574ae6429acec3807005ad12ab4
Руслан <fantom203232@gmail.com>
2025-05-12 14:42:18 +0500

    Update urls.py

--- clients/urls.py
+++ clients/urls.py
@@ -4,11 +4,20 @@ from clients.views import (
     RegistrationView, 
     LoginView,
     LogoutView,
+    ActivationView,
+    ProfileView,
+    DeleteProfileView
 )
 
 
 urlpatterns = [
     path(route="reg/", view=RegistrationView.as_view(), name="reg"),
     path(route="login/", view=LoginView.as_view(), name="login"),
-    path(route="logout/", view=LogoutView.as_view(), name="logout")
+    path(route="logout/", view=LogoutView.as_view(), name="logout"),
+    path(
+        route="activation/<str:username>/<str:code>", 
+        view=ActivationView.as_view(), name="activate"
+    ),
+    path(route="profile/", view=ProfileView.as_view(), name="profile"),
+    path(route="profile/delete/", view=DeleteProfileView.as_view(), name="delete_profile"),
 ]
